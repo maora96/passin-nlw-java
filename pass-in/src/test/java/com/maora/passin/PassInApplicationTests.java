c950f0733c0c2db4d5744cb6a083381368d562da
maora <marinaoliveira.r07@gmail.com>
2024-04-04 09:51:18 -0300

    chore: project setup

---
+++ pass-in/src/test/java/com/maora/passin/PassInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maora.passin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
